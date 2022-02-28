295f0399773b652a0e86fc8555250c1a356677a0
Demon-Wang <568654196@qq.com>
2022-02-28 23:53:02 +0800

    fix bug\

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -7522,7 +7522,7 @@ EXP_ST void setup_dirs_fds(void) {
 
   fprintf(plot_file, "# unix_time, cycles_done, cur_path, paths_total, "
                      "pending_total, pending_favs, map_size, unique_crashes, "
-                     "unique_hangs, max_depth, execs_per_sec\n");
+                     "unique_hangs, max_depth, execs_per_sec, total_execs, edges_found\n");
                      /* ignore errors */
 
 }
